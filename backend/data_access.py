e49f26c672517f00644a95460122b0220cb34959
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2025-03-22 00:05:29 -0400

    hashing functions, unimplemented for now

--- backend/data_access.py
+++ backend/data_access.py
@@ -3,6 +3,23 @@ from utils.firebase import db
 from firebase_admin import firestore
 import uuid
 
+def email_to_uid(email: str):
+    """
+    uniquely hash email to a user id
+    """
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, email))
+
+def title_to_postid(title: str, date):
+    """
+    uniquely hash title and created date to post id
+    """
+    unique_str = f"{title}_{date.isoformat()}"
+    
+    # Generate a deterministic UUID using the DNS namespace.
+    post_id = uuid.uuid5(uuid.NAMESPACE_DNS, unique_str)
+    
+    return str(post_id)
+
 class FirebaseDataAccess:
     """
     Get and upload to database

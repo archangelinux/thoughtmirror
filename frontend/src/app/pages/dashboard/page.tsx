bec3e6dbd75128b54cc23f70e7e7359d5ed11b2c
armaganngul <armagan.gul@mail.utoronto.ca>
2025-03-23 00:40:04 -0400

    about page update

--- frontend/src/app/pages/dashboard/page.tsx
+++ frontend/src/app/pages/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { color, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 import Calendar from "@/app/components/calendar";
 
